6546edd07d7e96d5a78f8413eb6ea1b0dcb59c60
Oleg Sych <olegsych@users.noreply.github.com>
2021-09-05 16:24:40 -0700

    Fix flaky ClockTest

--- tests/ClockTest.cs
+++ tests/ClockTest.cs
@@ -7,13 +7,15 @@ namespace Chronology
     {
         readonly IClock sut = new Clock();
 
+        static readonly TimeSpan clockPrecision = TimeSpan.FromMilliseconds(16);
+
         public class Time: ClockTest
         {
             [Fact]
             public void ReturnsCurrentUtcDateTime() {
                 DateTime expected = DateTime.UtcNow;
                 UtcDateTime actual = sut.Time;
-                Assert.Equal(expected, actual, TimeSpan.FromMilliseconds(1));
+                Assert.Equal(expected, actual, clockPrecision);
             }
         }
     }
